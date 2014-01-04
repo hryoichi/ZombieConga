f3e208a93725b70d7f79f066c637026822f8d78e
Ryoichi Hara <rychhr@gmail.com>
2014-01-05 01:04:36 +0900

    Add touch events to move a zombie

--- ZombieConga/MyScene.m
+++ ZombieConga/MyScene.m
@@ -42,8 +42,33 @@
     }
     _lastUpdateTime = currentTime;
 
-    [self moveSprite:_zombie velocity:CGPointMake(ZOMBIE_MOVE_POINTS_PER_SEC, 0)];
+    [self moveSprite:_zombie velocity:_velocity];
 }
+
+#pragma mark - Touch Events
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self];
+    [self moveZombieToward:touchLocation];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self];
+    [self moveZombieToward:touchLocation];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInNode:self];
+    [self moveZombieToward:touchLocation];
+}
+
+#pragma mark - Private
 
 - (void)moveSprite:(SKSpriteNode *)sprite velocity:(CGPoint)velocity
 {
@@ -51,6 +76,19 @@
 
     sprite.position = CGPointMake(sprite.position.x + amountToMove.x,
                                   sprite.position.y + amountToMove.y);
+}
+
+- (void)moveZombieToward:(CGPoint)location
+{
+    CGPoint offset = CGPointMake(location.x - _zombie.position.x,
+                                 location.y - _zombie.position.y);
+    CGFloat length = sqrtf(offset.x * offset.x + offset.y * offset.y);
+
+    // Normalizing a vector
+    CGPoint direction = CGPointMake(offset.x / length, offset.y / length);
+
+    _velocity = CGPointMake(direction.x * ZOMBIE_MOVE_POINTS_PER_SEC,
+                            direction.y * ZOMBIE_MOVE_POINTS_PER_SEC);
 }
 
 @end
