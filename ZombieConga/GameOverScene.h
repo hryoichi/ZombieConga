13c2dc08800836a673b3df982fcd998ca7a78167
Ryoichi Hara <rychhr@gmail.com>
2014-05-11 21:13:05 +0900

    Add GameOverScene

---
+++ ZombieConga/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  ZombieConga
+//
+//  Created by Ryoichi Hara on 2014/05/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import SpriteKit;
+
+@interface GameOverScene : SKScene
+
+@end
---
+++ ZombieConga/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  ZombieConga
+//
+//  Created by Ryoichi Hara on 2014/05/11.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
