3ad0d062da192cce0cd6cc995932553a8e6c536c
Ryoichi Hara <rychhr@gmail.com>
2014-05-12 11:26:04 +0900

    Add MainMenuScene

---
+++ ZombieConga/MainMenuScene.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuScene.h
+//  ZombieConga
+//
+//  Created by Ryoichi Hara on 2014/05/12.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+@import SpriteKit;
+
+@interface MainMenuScene : SKScene
+
+@end
---
+++ ZombieConga/MainMenuScene.m
@@ -0,0 +1,13 @@
+//
+//  MainMenuScene.m
+//  ZombieConga
+//
+//  Created by Ryoichi Hara on 2014/05/12.
+//  Copyright (c) 2014年 Ryoichi Hara. All rights reserved.
+//
+
+#import "MainMenuScene.h"
+
+@implementation MainMenuScene
+
+@end
